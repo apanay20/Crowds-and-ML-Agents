7e3cf08045f3aa978056faae19950566be8e48da
tkyria06 <55096410+tkyria06@users.noreply.github.com>
2021-06-02 20:19:32 +0300

    ML Agent - General Training (not completed training)

---
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    public float moveSpeed;
+
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+
+
+    [SerializeField] private Transform targetTransform;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+        Debug.Log(moveX);
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+
+    //just for testing
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+
+    }
+}
