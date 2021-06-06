aeb00ac38bea2a797ed8b1008e3d0917ecc31b3d
apanay20 <55096402+apanay20@users.noreply.github.com>
2021-06-06 21:14:37 +0300

    Obstacles

---
+++ Assets/Scripts/AddObstacleName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddObstacleName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.GetChild(0).gameObject.GetComponentInChildren<Button>().GetComponentInChildren<Text>().text = this.name;
+    }
+}
