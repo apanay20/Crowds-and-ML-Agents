d3da8a58a4f2429093ac31b74eb54e2f803ec977
apanay20 <55096402+apanay20@users.noreply.github.com>
2021-06-17 01:52:04 +0300

    Visual Additions

--- Assets/Scripts/LearnCustomScripts/MoveAgentLearn.cs
+++ Assets/Scripts/LearnCustomScripts/MoveAgentLearn.cs
@@ -9,14 +9,18 @@ public class MoveAgentLearn : MonoBehaviour
 
     private int localCounter = 1;
     private LoadDataLearn.AgentData agentData;
+    public Color color;
     public float speed;
     public Vector3 forward;
     public Vector3 goalVector;
     public float angle;
+    public bool walkNear;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        this.walkNear = false;
         this.name = this.agentData.name;
         setAgentColor();
         this.transform.GetChild(0).gameObject.GetComponentInChildren<Button>().GetComponentInChildren<Text>().text = this.name.Split('_')[1];
@@ -42,6 +46,21 @@ public class MoveAgentLearn : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Agent")
+        {
+            MoveAgentLearn colliderAgent = collision.gameObject.GetComponent<MoveAgentLearn>();
+            float movingAngle = Vector3.Angle(this.forward, colliderAgent.forward);
+            float distanceBetween = Vector3.Distance(this.transform.position, collision.gameObject.transform.position);
+            int counterDifference = Mathf.Abs(this.localCounter - colliderAgent.localCounter);
+            if (movingAngle < 10f && distanceBetween <= 1.2f && this.localCounter < 40) {
+                this.walkNear = true;
+                colliderAgent.walkNear = true;
+            }
+        }
+    }
+
     public void setAgentData(LoadDataLearn.AgentData data)
     {
         this.agentData = data;
@@ -49,7 +68,7 @@ public class MoveAgentLearn : MonoBehaviour
 
     private void visualizeLines()
     {
-        this.forward = transform.TransformDirection(Vector3.forward) * 3;
+        this.forward = transform.TransformDirection(Vector3.forward) * 1.6f;
         Debug.DrawRay(transform.position, this.forward, Color.white);
         this.goalVector = this.agentData.goalPos - transform.position;
         float m = this.forward.magnitude / this.goalVector.magnitude;
@@ -63,12 +82,16 @@ public class MoveAgentLearn : MonoBehaviour
 
     private void setAgentColor()
     {
-        Color tailColor = new Color(
+        Color randomColor = new Color(
             UnityEngine.Random.Range(0.1f, 1f),
             UnityEngine.Random.Range(0.2f, 1f),
             UnityEngine.Random.Range(0.1f, 1f)
         );
-        this.transform.GetChild(1).gameObject.GetComponentInChildren<TrailRenderer>().endColor = tailColor;
-        this.GetComponent<Renderer>().material.SetColor("_Color", tailColor);
+        this.color = randomColor;
+        this.transform.GetChild(1).gameObject.GetComponentInChildren<TrailRenderer>().endColor = this.color;
+        this.transform.GetChild(2).gameObject.GetComponentInChildren<LineRenderer>().startColor = this.color;
+        this.transform.GetChild(2).gameObject.GetComponentInChildren<LineRenderer>().endColor = this.color;
+        this.GetComponent<Renderer>().material.SetColor("_Color", this.color);
+
     }
 }
---
+++ Assets/Scripts/LearnCustomScripts/RadiusGizmo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RadiusGizmo : MonoBehaviour
+{
+    public int segments = 50;
+    public float xradius = 2;
+    public float yradius = 2;
+    public MoveAgentLearn agent;
+    LineRenderer line;
+
+    void Start()
+    {
+        this.agent = this.transform.parent.gameObject.GetComponent<MoveAgentLearn>();
+        line = gameObject.GetComponent<LineRenderer>();
+        line.positionCount = segments + 1;
+        line.useWorldSpace = false;
+        CreatePoints();
+        InvokeRepeating("blink", 0, 0.25f);
+
+    }
+    private void CreatePoints()
+    {
+        float x;
+        float z;
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+            z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+
+            line.SetPosition(i, new Vector3(x, 0, z));
+
+            angle += (360f / segments);
+        }
+    }
+
+    private void blink()
+    {
+        if (this.agent.walkNear == true)
+        {
+            if (line.endWidth > 0.01f)
+            {
+                line.startWidth = 0.01f;
+                line.endWidth = 0.01f;
+            }
+            else
+            {
+                line.startWidth = 0.05f;
+                line.endWidth = 0.05f;
+            }
+        }
+    }
+}
