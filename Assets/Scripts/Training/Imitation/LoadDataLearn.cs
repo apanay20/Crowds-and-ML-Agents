6d5d35779d80c94455aeeec7ee84deaef66d9fc2
apanay20 <55096402+apanay20@users.noreply.github.com>
2021-07-20 23:37:30 +0300

    ZaraImitation

--- Assets/Scripts/Training/Imitation/LoadDataLearn.cs
+++ Assets/Scripts/Training/Imitation/LoadDataLearn.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Unity.MLAgents;
 
 public class LoadDataLearn : MonoBehaviour
 {
@@ -9,11 +10,17 @@ public class LoadDataLearn : MonoBehaviour
     public string path;
     [HideInInspector]
     public List<AgentData> data;
-    public float counter = 0;
+    private float counter = 0;
+    public GameObject agentPrefabDemo;
+    public int numOfAgents;
+    public float neighbourPercentage;
     public GameObject agentPrefab;
     private float minStartTime = 1000000;
     private float minSpeed;
     private float maxSpeed;
+    private List<GoalAndSpawn> goals;
+    public Transform activeAgents;
+    public Transform inactiveAgents;
 
     public class AgentData
     {
@@ -28,19 +35,40 @@ public class LoadDataLearn : MonoBehaviour
         public float minSpeed;
     }
 
+    private class GoalAndSpawn
+    {
+        public Collider goalCollider;
+        public List<Collider> spawnCollider;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        //Find goals areas in scene
+        this.goals = new List<GoalAndSpawn>();
+        GameObject parentGoal = GameObject.Find("GoalAreas");
+        for (int i = 0; i < parentGoal.transform.childCount; i++)
+        {
+            GameObject child = parentGoal.transform.GetChild(i).gameObject;
+            GoalAndSpawn temp = new GoalAndSpawn();
+            temp.goalCollider = child.GetComponent<Collider>();
+            temp.spawnCollider = new List<Collider>();
+            for (int j = 0; j < child.transform.childCount; j++)
+                temp.spawnCollider.Add(child.transform.GetChild(j).GetComponent<Collider>());
+            this.goals.Add(temp);
+        }
+
         if (this.isImitation == true)
         {
             readData();
             findMinMaxSpeed();
             this.counter = this.minStartTime;
-            Time.timeScale = 0.5f;// 0.5f;
+            Time.timeScale = 0.5f;
         }
         else
         {
-            Time.timeScale = 0.05f;
+            spawnInitialAgents();
+            Time.timeScale = 0.2f;
         }
     }
 
@@ -103,6 +131,90 @@ public class LoadDataLearn : MonoBehaviour
         return ((speed - this.minSpeed) / (this.maxSpeed - this.minSpeed));
     }
 
+    private void spawnInitialAgents()
+    {
+        for(int i=0; i<this.numOfAgents; i++)
+        {
+            // [0] goal Pos, [1] spawnPos
+            List<Vector3> points = generateGoalAndSpawnPoints(false);
+            GameObject newAgent = Instantiate(agentPrefab, points[1], Quaternion.identity);
+            newAgent.GetComponent<WalkGoalImitation>().startingPos = points[1];
+            newAgent.GetComponent<WalkGoalImitation>().goalPos = points[0];
+            newAgent.transform.SetParent(activeAgents);
+        }
+    }
+    
+    [HideInInspector]
+    List<GameObject> twoAgents = new List<GameObject>();
+    private void respawnAgents()
+    {
+        int childsNum = this.inactiveAgents.childCount;
+        
+        foreach (Transform child in this.inactiveAgents)
+        {
+            if (twoAgents.Count < 2)
+            {
+                float rd = Random.Range(0f, 100f);
+                if (rd <= this.neighbourPercentage)
+                    if(twoAgents.Contains(child.gameObject) == false)
+                        twoAgents.Add(child.gameObject);
+                else
+                {
+                    // [0] goal Pos, [1] spawnPos
+                    List<Vector3> points = generateGoalAndSpawnPoints(false);
+                    child.transform.SetParent(this.activeAgents);
+                    child.GetComponent<WalkGoalImitation>().startingPos = points[1];
+                    child.GetComponent<WalkGoalImitation>().goalPos = points[0];
+                    child.gameObject.SetActive(true);
+                }
+            }
+            // twoAgents found and ready to spawn together
+            else
+            {
+                // [0] goal Pos 1, [1] spawnPos 1, [2] goal Pos 2, [3] spawnPos 2
+                List<Vector3> points = generateGoalAndSpawnPoints(true);
+                // Spawn first Agent
+                twoAgents[0].transform.SetParent(this.activeAgents);
+                twoAgents[0].GetComponent<WalkGoalImitation>().startingPos = points[1];
+                twoAgents[0].GetComponent<WalkGoalImitation>().goalPos = points[0];
+                twoAgents[0].SetActive(true);
+                // Spawn second Agent
+                twoAgents[1].transform.SetParent(this.activeAgents);
+                twoAgents[1].GetComponent<WalkGoalImitation>().startingPos = points[3];
+                twoAgents[1].GetComponent<WalkGoalImitation>().goalPos = points[2];
+                twoAgents[1].SetActive(true);
+
+                //Clear List
+                twoAgents.Clear();
+            }
+        }
+
+        if (twoAgents.Count == 2)
+        {
+            List<Vector3> points = generateGoalAndSpawnPoints(true);
+            // Spawn first Agent
+            twoAgents[0].transform.SetParent(this.activeAgents);
+            twoAgents[0].GetComponent<WalkGoalImitation>().startingPos = points[1];
+            twoAgents[0].GetComponent<WalkGoalImitation>().goalPos = points[0];
+            twoAgents[0].SetActive(true);
+            // Spawn second Agent
+            twoAgents[1].transform.SetParent(this.activeAgents);
+            twoAgents[1].GetComponent<WalkGoalImitation>().startingPos = points[3];
+            twoAgents[1].GetComponent<WalkGoalImitation>().goalPos = points[2];
+            twoAgents[1].SetActive(true);
+
+            //Clear List
+            twoAgents.Clear();
+        }
+
+    }
+
+    private void Update()
+    {
+        if (this.isImitation == false)
+            respawnAgents();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -112,11 +224,139 @@ public class LoadDataLearn : MonoBehaviour
             {
                 if (Mathf.Approximately(agent.startTime, this.counter))
                 {
-                    GameObject newAgent = Instantiate(agentPrefab, agent.positions[0], Quaternion.identity);
+                    GameObject newAgent = Instantiate(agentPrefabDemo, agent.positions[0], Quaternion.identity);
                     newAgent.GetComponent<AssignData>().setAgentData(agent);
+                    newAgent.transform.SetParent(this.activeAgents);
                 }
             }
             counter += 4;
         }
     }
+
+    private List<Vector3> generateGoalAndSpawnPoints(bool isNeighbourPoints)
+    {
+        // [0] goal Pos, [1] spawnPos
+        List<Vector3> retPoints = new List<Vector3>();
+
+        float spawnDistanceLimit = Academy.Instance.EnvironmentParameters.GetWithDefault("distance", 0f);
+
+        // Sort list by collider size
+        this.goals.Sort(compareBySize);
+
+        //Select a goal area and a random spawn point in that area
+        int randomSpawnIndex = getRandomIndex(true);
+        Collider tempArea = goals[randomSpawnIndex].goalCollider;
+        Vector3 goalPoint = new Vector3(Random.Range(tempArea.bounds.min.x, tempArea.bounds.max.x), 0f, Random.Range(tempArea.bounds.min.z, tempArea.bounds.max.z));
+        Vector3 goalPoint2 = Vector3.zero;
+        //Select another goal point near the first
+        if (isNeighbourPoints == true)
+        {
+            do
+            {
+                goalPoint2 = new Vector3(Random.Range(tempArea.bounds.min.x, tempArea.bounds.max.x), 0f, Random.Range(tempArea.bounds.min.z, tempArea.bounds.max.z));
+            } while (Vector3.Distance(goalPoint, goalPoint2) > 0.5f);
+        }
+
+
+        //========================================= SELECT SPAWN POIN BELOW=================================================
+
+        Vector3 spawnPoint = Vector3.zero;
+        Vector3 spawnPoint2 = Vector3.zero;
+        Collider tempArea2;
+
+        if (spawnDistanceLimit == 0)
+        {
+            //No limit, select a random goal area
+            //Remove selected goal area so will not select is as goal area too
+            GoalAndSpawn tempBeforeRemove = this.goals[randomSpawnIndex];
+            this.goals.Remove(this.goals[randomSpawnIndex]);
+            randomSpawnIndex = getRandomIndex(false);
+            tempArea2 = this.goals[randomSpawnIndex].goalCollider;
+            spawnPoint = new Vector3(Random.Range(tempArea2.bounds.min.x, tempArea2.bounds.max.x), 0f, Random.Range(tempArea2.bounds.min.z, tempArea2.bounds.max.z));
+            this.goals.Add(tempBeforeRemove);
+        }
+        else
+        {
+            //Use current lesson's maximum spawn distance
+            float spawnToGoalDistance = 0f;
+            while (true)
+            {
+                List<Collider> tempSpawnAreas = this.goals[randomSpawnIndex].spawnCollider;
+                int randomSpawnArea = Random.Range(0, tempSpawnAreas.Count);
+                tempArea2 = tempSpawnAreas[randomSpawnArea];
+                spawnPoint = new Vector3(Random.Range(tempArea2.bounds.min.x, tempArea2.bounds.max.x), 0f, Random.Range(tempArea2.bounds.min.z, tempArea2.bounds.max.z));
+                spawnToGoalDistance = Vector3.Distance(spawnPoint, goalPoint);
+                if (spawnToGoalDistance <= spawnDistanceLimit && spawnToGoalDistance >= (spawnDistanceLimit - 0.5f))
+                    break;
+
+            }
+        }
+        //Select another spawn point near the first
+        if (isNeighbourPoints == true)
+        {
+            do
+            {
+                spawnPoint2 = new Vector3(Random.Range(tempArea2.bounds.min.x, tempArea2.bounds.max.x), 0f, Random.Range(tempArea2.bounds.min.z, tempArea2.bounds.max.z));
+            } while (Vector3.Distance(spawnPoint, spawnPoint2) > 0.5f);
+        }
+        
+        retPoints.Add(goalPoint);
+        retPoints.Add(spawnPoint);
+
+        if(isNeighbourPoints == true)
+        {
+            retPoints.Add(goalPoint2);
+            retPoints.Add(spawnPoint2);
+        }
+
+        return retPoints;
+    }
+
+    // Compare collider size
+    private int compareBySize(GoalAndSpawn a, GoalAndSpawn b)
+    {
+        Vector3 boundsA = a.goalCollider.bounds.size;
+        Vector3 boundsB = b.goalCollider.bounds.size;
+
+        float sizeA = boundsA.x * boundsA.z;
+        float sizeB = boundsB.x * boundsB.z;
+        return sizeA.CompareTo(sizeB);
+    }
+
+    private int getRandomIndex(bool isGoalPlace)
+    {
+        //Goal points selected randomly
+        if (isGoalPlace == true)
+            return Random.Range(0, this.goals.Count - 1);
+        // If is a spawn point, select random weighted relative to area size
+        int listSize = this.goals.Count;
+        List<float> colliderSize = new List<float>();
+        float maxSize = 0f;
+        float minSize = 100000f;
+
+        foreach (GoalAndSpawn c in this.goals)
+        {
+            float tempBoundSize = c.goalCollider.bounds.size.x * c.goalCollider.bounds.size.z;
+            if (tempBoundSize > maxSize)
+                maxSize = tempBoundSize;
+            if (tempBoundSize < minSize)
+                minSize = tempBoundSize;
+
+            colliderSize.Add(tempBoundSize);
+        }
+
+        int retIndex = 0;
+        float rd = Random.Range(minSize, maxSize);
+        for (int i = 0; i < colliderSize.Count; i++)
+        {
+            if (rd <= colliderSize[i])
+            {
+                retIndex = i;
+                break;
+            }
+        }
+        return retIndex;
+    }
 }
+
+
--- Assets/Scripts/Training/Imitation/WalkGoalImitation.cs
+++ Assets/Scripts/Training/Imitation/WalkGoalImitation.cs
@@ -14,7 +14,9 @@ public class WalkGoalImitation : Agent
     private Rigidbody agentRB;
     private float spawnDistanceLimit;
     public float currentAngle;
-    private Vector3 startingPos;
+    [HideInInspector]
+    public Vector3 startingPos;
+    [HideInInspector]
     public Vector3 goalPos;
     private float goalDistance;
     public float currentGoalDistance;
@@ -23,23 +25,61 @@ public class WalkGoalImitation : Agent
     private int localCounter = 1;
     private float moveSpeed;
     public float currentSpeed;
-    private List<GoalAndSpawn> goals;
     public GameObject neighbour;
-    private bool hasNeighbour;
+    [HideInInspector]
+    public bool hasNeighbour;
+    [HideInInspector]
+    public bool hasInteracted;
     private float neighAngle;
-    private int episodeActivated = -1;
+    [HideInInspector]
+    public int episodeActivated = -1;
+    public int currentEpisode;
+    public int currentStep;
 
-    /*
-    */
-
-    private class GoalAndSpawn
+    public override void Initialize()
     {
-        public Collider goalCollider;
-        public List<Collider> spawnCollider;
+        this.agentRB = this.GetComponent<Rigidbody>();
+        this.controller = GameObject.Find("Plane").GetComponent<LoadDataLearn>();
+        this.dataScript = this.GetComponent<AssignData>();
+        this.saveRouteScript = this.GetComponent<SaveRoute>();
+        
+        if(this.controller.isImitation == true)
+        {
+            this.name = this.dataScript.agentData.name;
+            this.nameText.text = int.Parse(this.name.Split('_')[1]).ToString();
+            this.moveSpeed = 100f;
+        }
+        else
+            this.moveSpeed = 50f;
+        setAgentColor();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        this.neighbour = null;
+        this.hasNeighbour = false;
+        this.hasInteracted = false;
+        this.episodeActivated = -1;
+
+        if(this.controller.isImitation == true)
+        {
+            this.startingPos = this.dataScript.agentData.positions[0];
+            this.goalPos = this.dataScript.agentData.goalPos;
+        }
+
+        transform.localPosition = this.startingPos;
+        transform.LookAt(this.goalPos);
+        float offset = Random.Range(-15f, 15f);
+        transform.Rotate(0f, transform.rotation.y + offset, 0f);
+
+        this.goalDistance = Vector3.Distance(transform.localPosition, this.goalPos);
+        this.transform.GetChild(1).GetComponent<TrailRenderer>().Clear();    
     }
 
     private void Update()
     {
+        this.currentEpisode = this.CompletedEpisodes;
+        this.currentStep = this.StepCount;
         this.reward = this.GetCumulativeReward();
         this.currentGoalDistance = Vector3.Distance(transform.localPosition, this.goalPos);
 
@@ -69,147 +109,6 @@ public class WalkGoalImitation : Agent
             if (this.neighbour != null)
                 Debug.DrawLine(transform.position, this.neighbour.transform.position, Color.red);
         }
-    }
-
-    public override void Initialize()
-    {
-        this.agentRB = this.GetComponent<Rigidbody>();
-        this.controller = GameObject.Find("Plane").GetComponent<LoadDataLearn>();
-        this.dataScript = this.GetComponent<AssignData>();
-        this.saveRouteScript = this.GetComponent<SaveRoute>();
-        this.moveSpeed = 50f;
-        
-        if(this.controller.isImitation == true)
-        {
-            this.name = this.dataScript.agentData.name;
-            this.nameText.text = int.Parse(this.name.Split('_')[1]).ToString();
-        }
-        else
-        {
-            //Find goals areas in scene
-            this.goals = new List<GoalAndSpawn>();
-            GameObject parentGoal = GameObject.Find("GoalAreas");
-            for (int i = 0; i < parentGoal.transform.childCount; i++)
-            {
-                GameObject child = parentGoal.transform.GetChild(i).gameObject;
-                GoalAndSpawn temp = new GoalAndSpawn();
-                temp.goalCollider = child.GetComponent<Collider>();
-                temp.spawnCollider = new List<Collider>();
-                for (int j = 0; j < child.transform.childCount; j++)
-                    temp.spawnCollider.Add(child.transform.GetChild(j).GetComponent<Collider>());
-                this.goals.Add(temp);
-            }
-        }
-        setAgentColor();
-    }
-
-    public override void OnEpisodeBegin()
-    {
-        this.spawnDistanceLimit = Academy.Instance.EnvironmentParameters.GetWithDefault("distance", 0f);
-        this.neighbour = null;
-        this.hasNeighbour = false;
-        this.episodeActivated = -1;
-        // If current run is imitation then get data from file, else create random points 
-        if (this.controller.isImitation == true)
-        {
-            this.startingPos = this.dataScript.agentData.positions[0];
-            this.goalPos = this.dataScript.agentData.goalPos;
-            transform.LookAt(this.goalPos);
-        }
-        else
-        {
-            this.startingPos = randomSpawnPoint();
-            transform.localPosition = this.startingPos;
-            transform.LookAt(this.goalPos);
-            float offset = Random.Range(-20f, 20f);
-            transform.Rotate(0f, transform.rotation.y + offset, 0f);
-        }
-        this.goalDistance = Vector3.Distance(transform.localPosition, this.goalPos);
-        this.transform.GetChild(1).GetComponent<TrailRenderer>().Clear();    
-    }
-
-    private Vector3 randomSpawnPoint()
-    {
-        // Sort list by collider size
-        this.goals.Sort(compareBySize);
-
-        //Select a goal area and a random spawn point in that area
-        int randomSpawnIndex = getRandomIndex();
-        Collider tempArea = goals[randomSpawnIndex].goalCollider;
-        this.goalPos = new Vector3(Random.Range(tempArea.bounds.min.x, tempArea.bounds.max.x), 0f, Random.Range(tempArea.bounds.min.z, tempArea.bounds.max.z));
-
-        Vector3 spawnPoint = Vector3.zero;
-
-        if (spawnDistanceLimit == 0)
-        {
-            //No limit, select a random goal area
-            //Remove selected goal area so will not select is as goal area too
-            GoalAndSpawn tempBeforeRemove = this.goals[randomSpawnIndex];
-            this.goals.Remove(this.goals[randomSpawnIndex]);
-            randomSpawnIndex = getRandomIndex();
-            Collider tempArea2 = this.goals[randomSpawnIndex].goalCollider;
-            spawnPoint = new Vector3(Random.Range(tempArea2.bounds.min.x, tempArea2.bounds.max.x), 0f, Random.Range(tempArea2.bounds.min.z, tempArea2.bounds.max.z));
-            this.goals.Add(tempBeforeRemove);
-        }
-        else
-        {
-            //Use current lesson's maximum spawn distance
-            float spawnToGoalDistance = 0f;
-            while (true)
-            {
-                List<Collider> tempSpawnAreas = this.goals[randomSpawnIndex].spawnCollider;
-                int randomSpawnArea = Random.Range(0, tempSpawnAreas.Count);
-                Collider tempArea2 = tempSpawnAreas[randomSpawnArea];
-                spawnPoint = new Vector3(Random.Range(tempArea2.bounds.min.x, tempArea2.bounds.max.x), 0f, Random.Range(tempArea2.bounds.min.z, tempArea2.bounds.max.z));
-                spawnToGoalDistance = Vector3.Distance(spawnPoint, this.goalPos);
-                if (spawnToGoalDistance <= spawnDistanceLimit && spawnToGoalDistance >= (spawnDistanceLimit - 0.5f))
-                    break;
-
-            }
-        }
-        return spawnPoint;
-    }
-
-    // Compare collider size
-    private int compareBySize(GoalAndSpawn a, GoalAndSpawn b)
-    {
-        Vector3 boundsA = a.goalCollider.bounds.size;
-        Vector3 boundsB = b.goalCollider.bounds.size;
-
-        float sizeA = boundsA.x * boundsA.z;
-        float sizeB = boundsB.x * boundsB.z;
-        return sizeA.CompareTo(sizeB);
-    }
-
-    private int getRandomIndex()
-    {
-        int listSize = this.goals.Count;
-        List<float> colliderSize = new List<float>();
-        float maxSize = 0f;
-        float minSize = 100000f;
-
-        foreach (GoalAndSpawn c in this.goals)
-        {
-            float tempBoundSize = c.goalCollider.bounds.size.x * c.goalCollider.bounds.size.z;
-            if (tempBoundSize > maxSize)
-                maxSize = tempBoundSize;
-            if (tempBoundSize < minSize)
-                minSize = tempBoundSize;
-
-            colliderSize.Add(tempBoundSize);
-        }
-
-        int retIndex = 0;
-        float rd = Random.Range(minSize, maxSize);
-        for (int i = 0; i < colliderSize.Count; i++)
-        {
-            if (rd <= colliderSize[i])
-            {
-                retIndex = i;
-                break;
-            }                
-        }
-        return retIndex;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -242,9 +141,10 @@ public class WalkGoalImitation : Agent
         if (this.hasNeighbour == true)
         {
             float neighDistance = Vector3.Distance(transform.position, this.neighbour.transform.position);
-            neighDistance = Mathf.Clamp(neighDistance, 1f, float.MaxValue);
-            float rewardNeighbour = 0.0001f + (0.0001f / neighDistance);
-            AddReward(rewardNeighbour);
+            if(neighDistance <= 1.2f)
+                AddReward(+0.0001f);
+            else
+                AddReward(-0.0002f);
         }
 
         // Add small punishment in every step
@@ -259,7 +159,7 @@ public class WalkGoalImitation : Agent
         if (direction == 0)
             angle = -angle;
 
-        if (this.controller.isImitation == false)
+        if(this.controller.isImitation == false)
             angle *= 10f;
 
         transform.Rotate(0f, angle, 0f);
@@ -308,11 +208,9 @@ public class WalkGoalImitation : Agent
 
     private void disableAgent()
     {
-        if (this.controller.isImitation == true)
-        {
-            this.GetComponent<DemonstrationRecorder>().Close();
-            this.gameObject.SetActive(false); 
-        }
+        this.GetComponent<DemonstrationRecorder>().Close();
+        transform.SetParent(GameObject.Find("InactiveAgents").transform);
+        this.gameObject.SetActive(false); 
     }
 
     private void OnTriggerEnter(Collider other)
@@ -341,17 +239,24 @@ public class WalkGoalImitation : Agent
     {
         if (other.tag == "Agent")
         {
-            if (this.neighbour == null)
+            if (this.neighbour == null && this.hasInteracted == false)
             {
-                if (StepCount > 10f)
+                if (other.GetComponent<WalkGoalImitation>().hasInteracted == false)
                 {
                     this.neighAngle = Vector3.Angle(transform.forward, other.transform.forward);
-                    float neighGoalDistance = Vector3.Distance(this.goalPos, other.gameObject.GetComponent<WalkGoalImitation>().goalPos);
-                    if (this.neighAngle <= 30f && neighGoalDistance <= 5f)
+                    WalkGoalImitation otherScript = other.gameObject.GetComponent<WalkGoalImitation>();
+                    float neighGoalDistance = Vector3.Distance(this.goalPos, otherScript.goalPos);
+                    if (this.neighAngle <= 30f && neighGoalDistance <= 3f)
                     {
                         this.neighbour = other.gameObject;
                         this.hasNeighbour = true;
+                        this.hasInteracted = true;
                         this.episodeActivated = other.GetComponent<Agent>().CompletedEpisodes;
+
+                        otherScript.neighbour = this.gameObject;
+                        otherScript.hasNeighbour = true;
+                        otherScript.hasInteracted = true;
+                        otherScript.episodeActivated = this.CompletedEpisodes;
                     }
                 }
             }
@@ -360,17 +265,6 @@ public class WalkGoalImitation : Agent
         {
             AddReward(-0.0001f);
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Agent")
-            if(other.gameObject == this.neighbour)
-            {
-                this.hasNeighbour = false;
-                this.neighbour = null;
-                this.episodeActivated = -1;
-            }
     }
 
     // Get next point direction
